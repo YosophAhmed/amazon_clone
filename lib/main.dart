dc69721c4ce74163e8c3c2ecfe557ebf42c0e4cd
Yosoph <devyosoph@gmail.com>
2023-09-21 16:45:20 +0300

    App Router

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:amazon_clone/constants/app_colors.dart';
+import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone/router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,10 +12,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      title: 'Amazon CLone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: AppColors.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen(),
     );
   }
 }
-
---
+++ lib/router.dart
@@ -0,0 +1,23 @@
+import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+Route<dynamic> generateRoute(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
+    case AuthScreen.routeName:
+      return MaterialPageRoute(
+        builder: (BuildContext context) => const AuthScreen(),
+        settings: routeSettings,
+      );
+    default:
+      return MaterialPageRoute(
+        builder: (BuildContext context) => const Scaffold(
+          body: Center(
+            child: Text(
+              'Screen does not exist',
+            ),
+          ),
+        ),
+        settings: routeSettings,
+      );
+  }
+}
