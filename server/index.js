35408e77640c0e7236a522366f901fc348087445
Yosoph <devyosoph@gmail.com>
2023-09-24 13:23:39 +0300

    Api SignUp

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const authRouter = require("./routes/auth");
 
 const PORT = 3000;
 const app = express();
+const url = "";
 
+app.use(express.json());
 app.use(authRouter);
+
+mongoose.connect(url).then(() => {
+    console.log("Connection Successful");
+}).catch((error => {
+    console.log(error);
+}));
 
 app.listen(
     PORT,
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,29 @@
 const express = require("express");
+const User = require("../models/user");
+const bcryptjs = require('bcryptjs');
+
 
 const authRouter = express.Router();
 
-authRouter.get(
-    "/user",
-    (request,response) => {
-    response.json({
-    message: "Yosoph"
-    });
+authRouter.post('/api/signup', async (request, response) => {
+    try{
+        const {name, email, password} = request.body;
+            const existingUser = await User.findOne({email});
+            if(existingUser) {
+                return response.status(400).json({message: 'User with same email already exists'});
+            }
+            const hashedPassword = await bcryptjs.hash(password, 8);
+            let user = new User({
+                email,
+                password: hashedPassword,
+                name,
+            });
+            user = await user.save();
+            response.json(user);
+    } catch (error) {
+        response.status(500).json({error: error.message});
     }
-);
+
+});
 
 module.exports = authRouter;
